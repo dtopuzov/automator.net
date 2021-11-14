fb01702dc1bc437b114d1f0705eb9d45df64cc1e
Dimitar Topuzov <dtopuzov@gmail.com>
2021-11-14 21:09:47 +0200

    Update install-calc.ps1

--- setup/install-calc.ps1
+++ setup/install-calc.ps1
@@ -1,6 +1,12 @@
+# Download winget
 $source = 'https://github.com/microsoft/winget-cli/releases/download/v1.1.12653/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
 $destination = 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
 Invoke-RestMethod -Uri $source -OutFile $destination
+
+# Install winget
+Import-Module -Name Appx -UseWindowsPowerShell -WarningAction SilentlyContinue
 Add-AppPackage -path $destination
+
+# Install Windows Calculator
 winget install "Windows Calculator" --force --silent --accept-package-agreements
 Remove-Item "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -Force -Recurse
