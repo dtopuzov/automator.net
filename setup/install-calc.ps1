d4a0d0078737cad9a4c0519fba1a10f98e7bd067
Dimitar Topuzov <dtopuzov@gmail.com>
2021-11-14 21:02:49 +0200

    Create install-calc.ps1

---
+++ setup/install-calc.ps1
@@ -0,0 +1,6 @@
+$source = 'https://github.com/microsoft/winget-cli/releases/download/v1.1.12653/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
+$destination = 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
+Invoke-RestMethod -Uri $source -OutFile $destination
+Add-AppPackage -path $destination
+winget install "Windows Calculator" --force --silent --accept-package-agreements
+Remove-Item "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -Force -Recurse
