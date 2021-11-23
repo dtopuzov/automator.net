f86dbd1f7e52c9e6521ecf4947d938517365b6c3
Dimitar Topuzov <dtopuzov@gmail.com>
2021-11-23 14:15:15 +0200

    chore: add mobile tests

---
+++ setup/windows.bat
@@ -0,0 +1,3 @@
+call npm i -g appium@2.0.0-beta.19
+call appium driver install uiautomator2
+call appium driver install windows
--- src/Automator.Tests.Mobile/Tests/VisualTests.cs
+++ src/Automator.Tests.Mobile/Tests/VisualTests.cs
@@ -1,13 +1,26 @@
 using NUnit.Framework;
+using OpenQA.Selenium.Appium;
 
 namespace Automator.Tests.Mobile.Tests
 {
     public class VisualTests : BaseTest
     {
         [Test]
-        public void ShouldLookOK()
+        public void HomeShouldLookOK()
         {
-            Assert.Pass();
+            App.Click(MobileBy.AccessibilityId("Home"));
+
+            var looksOK = App.CompareScreen("home-page", tolerance: 0.1);
+            Assert.True(looksOK, "Home page does not look ok.");
+        }
+
+        [Test]
+        public void LoginShouldLookOK()
+        {
+            App.Click(MobileBy.AccessibilityId("Login"));
+
+            var looksOK = App.CompareScreen("login-page", tolerance: 0.1);
+            Assert.True(looksOK, "Login page does not look ok.");
         }
     }
 }
